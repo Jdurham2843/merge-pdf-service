0705afe942b59bd853f839b37b808b668ace5abb
jdurham2843 <jdurham2843@gmail.com>
2016-08-19 21:18:42 -0500

    Can send merged file to client-side

--- merge-app.py
+++ merge-app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, send_from_directory, current_app
 from werkzeug.utils import secure_filename
+
+import merge
 
 ALLOWED_EXTENSIONS = set(['pdf'])
 UPLOAD_FOLDER = 'uploads/'
@@ -25,17 +27,40 @@ def index_page():
         return render_template('index.html')
 
     elif request.method == 'POST':
+        os.remove('uploads/merged.pdf')
         files = request.files.getlist('file[]', None)
         filenames = []
+        flash_message = ''
+        file_was_merged = False
         if not files:
             flash('No file part')
-            return render_template('index.html')
+            return render_template('index.html', file_was_merged=file_was_merged)
         for file in files:
             if allowed_file(file.filename):
                 filenames.append(file.filename)
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
-        
-        file_message = request.form['merge-order'] if request.form['merge-order'] else ''
-        flash(file_message)
-        return render_template('index.html')
+            else:
+                flash_message = '%s is of a file type that is not allowed.' % (file.filename)
+        file_list_ordered = request.form.get('merge-order').split(',')
+        file_list_ordered = merge.format_files(file_list_ordered)
+
+        if flash_message == '' and file_list_ordered != '':
+            try:
+                merge.merge(file_list_ordered)
+            except Exception as e:
+                flash_message = str(e)
+            else:
+                file_was_merged = True
+
+        if flash_message:
+            flash(flash_message)
+        else:
+            uploads = os.path.join(os.path.dirname(os.path.realpath(__file__)),  app.config['UPLOAD_FOLDER'])
+            flash(uploads)
+        return render_template('index.html', file_was_merged=file_was_merged)
+
+@app.route('/download/', methods=['GET'])
+def download_file():
+    #uploads = os.path.join(current_app.root_path,  app.config['UPLOAD_FOLDER'])
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename='merged.pdf', as_attachment=True)
--- merge.py
+++ merge.py
@@ -1,30 +1,42 @@
 import sys
+import os
 import re
 from PyPDF2 import PdfFileMerger
 
 def merge(files):
-    check_if_pdfs(files)
     merger = PdfFileMerger()
 
     for file_name in files:
         try:
-            opened = open(file_name, 'rb')
+            opened = open('uploads/%s' % (file_name), 'rb')
         except:
-            print('%s could not be opened :/' % file_name)
-            raise
+            raise Exception('uploads/%s' % (file_name))
         else:
-            merger.append(file_name)
-            opened.close()
+            merger.append(opened)
 
-    with open('merged.pdf', 'wb') as final_file:
+    with open('uploads/merged.pdf', 'wb') as final_file:
         merger.write(final_file)
 
-def check_if_pdfs(files):
-    pattern = '.+\.pdf'
-    for file_name in files:
-        if not re.match(pattern, file_name):
-            raise Exception('File is not a pdf file')
+    cleanup_uploads_folder(files)
 
+def cleanup_uploads_folder(files):
+        """ This function cleanups up the uploads folder"""
+        for filename in files:
+                os.remove('uploads/%s' % filename)
+
+def format_files(files):
+    delim = {' ', '(', ')', '{', '}', '[', ']'}
+    files_formatted = []
+    for file_name in files:
+        file_name_formatted = []
+        for char in file_name:
+            if char not in delim:
+                file_name_formatted.append(char)
+            elif char == ' ':
+                file_name_formatted.append('_')
+        file_name_formatted_str = ''.join(file_name_formatted)
+        files_formatted.append(file_name_formatted_str)
+    return files_formatted
 
 if __name__ == '__main__':
     merge(sys.argv[1:])
